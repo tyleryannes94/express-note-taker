c5c6139649f8061baa37769b4713d33c36b8d896
tyleryannes94 <tyler.yannes@gmail.com>
2023-12-18 20:43:54 -0600

    Added the app.get function to parse the returned JSON data from /notes url.

--- app.js
+++ app.js
@@ -8,3 +8,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+app.get ('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json','utf8',(err,data) =>{
+        if (err) throw err;
+        res.json (JSON.parse(data));
+    })
+});
