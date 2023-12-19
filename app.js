42a46b2e355fd4df30cf5b72a876ab78f3b6d63f
tyleryannes94 <tyler.yannes@gmail.com>
2023-12-18 20:35:47 -0600

    Setup express code in app.js.

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ const path = require ('path');
 
 const app = express ();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
